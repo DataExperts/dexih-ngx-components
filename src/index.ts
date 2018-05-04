19541c6c93a1c87aead6bdbe97926b00af0927f0
garyholland <gholland@dataexpertsgroup.com>
2018-05-04 13:52:40 +1000

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,6 @@
+// need to export all components for AOT builds.
+
 export * from './dexih-components.module';
 export * from './widget/index';
 export * from './buttons/index';
 export * from './form-controls/index';
-
-// export { DexihComponentsModule } from './dexih-components.model';
-
-// export {
-//   DexihWidgetModule,
-//   DexihWidgetComponent,
-//   DexihWidgetColumnsComponent,
-//   DexihWidgetDeckComponent,
-//   DexihWidgetVerticalComponent
-// } from './widget';
-
-// export {
-//   DexihButtonsModule,
-//   DexihButtonComponent,
-//   DexihButtonCancelComponent,
-//   DexihButtonApplyComponent,
-//   DexihButtonNewComponent,
-//   DexihButtonDeleteComponent,
-//   DexihButtonSaveComponent,
-//   DexihButtonSaveLocalComponent,
-//   DexihButtonEditComponent,
-//   DexihButtonPreviewComponent,
-//   DexihButtonViewComponent,
-//   DexihButtonExportComponent,
-//   DexihButtonDownloadComponent,
-//   DexihButtonRefreshComponent,
-//   DexihButtonDropDownComponent,
-//   DexihButtonCloseComponent,
-//   DexihButtonOpenComponent,
-//   DexihButtonShareComponent,
-//   DexihButtonPrivateComponent,
-//   DexihButtonMoreComponent,
-//   DexihButtonSplitDropDownComponent,
-//   DexihButtonValidateComponent,
-//   DexihButtonHistoryComponent,
-//   DexihButtonLinkComponent
-// } from './buttons';
-
-// export {
-//   DexihFormControlsModule,
-//   DexihFormInputComponent,
-//   DexihFormDateComponent,
-//   DexihFormTimeComponent,
-//   DexihFormTextAreaComponent,
-//   DexihFormSelectComponent,
-//   DexihFormCheckboxComponent,
-//   DexihFormDaysOfWeekComponent,
-//   DexihFormTagsComponent,
-//   DexihFormTagsDropdownComponent,
-//   DexihSelectFilterPipe,
-// } from './form-controls';
