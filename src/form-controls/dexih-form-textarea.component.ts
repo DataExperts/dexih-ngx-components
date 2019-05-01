51a8135a737f7c832513f00c9fc37894f72bc6b3
garyholland <gholland@dataexpertsgroup.com>
2019-05-01 11:55:49 +1000

    improved copy button on form-text

--- src/form-controls/dexih-form-textarea.component.ts
+++ src/form-controls/dexih-form-textarea.component.ts
@@ -82,9 +82,17 @@ export class DexihFormTextAreaComponent implements OnInit, ControlValueAccessor 
         }
     }
 
-    copy() {
-        let textArea = <HTMLTextAreaElement> document.getElementById(this.id);
-        textArea.select();
+    copyMessage() {
+        let selBox = document.createElement('textarea');
+        selBox.style.position = 'fixed';
+        selBox.style.left = '0';
+        selBox.style.top = '0';
+        selBox.style.opacity = '0';
+        selBox.value = this.value;
+        document.body.appendChild(selBox);
+        selBox.focus();
+        selBox.select();
         document.execCommand('copy');
-    }
+        document.body.removeChild(selBox);
+      }
 }
